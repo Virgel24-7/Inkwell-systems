47da81d0cace632417037a5d27cdd73976d91bfe
icecoldboi00 <166093123+icecoldboi00@users.noreply.github.com>
2024-04-10 20:17:42 +0800

    Add files via upload

---
+++ src/firebase-config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from '@firebase/firestore'
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDNJQ3mVwHd3UQfHpABczul94S7idozL1c",
+    authDomain: "inkwell-sys.firebaseapp.com",
+    projectId: "inkwell-sys",
+    storageBucket: "inkwell-sys.appspot.com",
+    messagingSenderId: "160191100269",
+    appId: "1:160191100269:web:33a4f759c15457bc451664",
+    measurementId: "G-L3HLG1MBMY"
+  };
+
+  const app = initializeApp(firebaseConfig);
+
+  export const db = getFirestore()
+
+
