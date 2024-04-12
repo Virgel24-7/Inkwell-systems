88c59fc4ef6bdf49eac35d106449f4dd86b7321d
Virgel24-7 <158011872+Virgel24-7@users.noreply.github.com>
2024-04-12 23:34:18 +0800

    added library prototype class
    optimized book display by implementing lists in React,
    
    library exports book array which is mapped to get div component for bookcards
    
    planning to implement prototype firebase books tomorrow

--- src/Bookpage.jsx
+++ src/Bookpage.jsx
@@ -1,4 +1,5 @@
 import { Bookcard } from "./Bookcard";
+import library from "./Library";
 
 export const Bookpage = () => {
   return (
@@ -19,41 +20,24 @@ export const Bookpage = () => {
       <br />
       <br />
 
-      <div className="books">
-        <Bookcard
-          title="Harry Potter"
-          description="Harry Potter is a series of seven fantasy novels written by British author J. K. Rowling. 
-                            The first novel in the series is Harry Potter and the Philosophers Stone."
-          image="harryPotter.jpg"
-        />
-        <Bookcard
-          title="Blue Lock"
-          description="Blue Lock is a Japanese football manga series written by Muneyuki Kaneshiro and illustrated by Yusuke Nomura."
-          image="blueLock.jpg"
-        />
-        <Bookcard
-          title="Solo Leveling"
-          description="The story is set in a world where humans, 
-                            known as hunters, who possess supernatural abilities, must battle deadly monsters to protect mankind."
-          image="soloLeveling.jpg"
-        />
-        <Bookcard
-          title="Initial D"
-          description="A Japanese street racing manga series written and illustrated by Shuichi Shigeno."
-          image="initiald.jpg"
-        />
-        <Bookcard
-          title="One Piece"
-          description="One Piece is a manga and anime series that follows Monkey D. Luffy,
-                            a young boy with a dream to become the greatest pirate in the world."
-          image="onepiece.jpg"
-        />
-        <Bookcard
-          title="Hajime no ippo"
-          description="A Japanese boxing -themed manga series written and illustrated by George Morikawa."
-          image="hajime.jpg"
-        />
-      </div>
+      <Bookpagemain />
+    </div>
+  );
+};
+
+const Bookpagemain = () => {
+  return (
+    <div className="books">
+      {library.map((book, key) => {
+        return (
+          <Bookcard
+            key={key}
+            title={book.title}
+            description={book.description}
+            image={book.image}
+          />
+        );
+      })}
     </div>
   );
 };
