fd8caea00cafd9fd163a7ee1665b65797dfc2e51
Virgel24-7 <158011872+Virgel24-7@users.noreply.github.com>
2024-04-13 22:42:20 +0800

    implemented get image in firestore
    used systematic naming (camel case the book title) as a jpg file in firebase cloud storage, name of the image is stored in image field for each book document in the booksdemo collection

--- src/Bookcard.jsx
+++ src/Bookcard.jsx
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react";
+import { storage } from "./firebase-config";
+import { getDownloadURL, ref } from "firebase/storage";
+
 export const Bookcard = (props) => {
+  const [imgUrl, setImgUrl] = useState("");
+
+  useEffect(() => {
+    const getImage = async () => {
+      const reference = ref(storage, `/bookcovers/${props.image}`);
+
+      await getDownloadURL(reference).then((x) => {
+        setImgUrl(x);
+      });
+    };
+
+    getImage();
+  }, []);
+
   return (
     <div className="card">
       <div className="imgBx">
         <a href="#">
-          <img src={"src/assets/" + props.image} />
+          <img src={imgUrl} />
         </a>
       </div>
       <h2>{props.title}</h2>
