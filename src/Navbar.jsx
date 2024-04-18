3bdcb3a13391e483fd7e439777de9ba8c05ef51b
ShinXplorer <166099282+ShinXplorer@users.noreply.github.com>
2024-04-18 15:23:05 +0800

    Update Navbar.jsx
    Responsive navbar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,33 +1,44 @@
 import { Link } from "react-router-dom";
+import  {useState} from 'react';
 
 export const Navbar = () => {
-  return (
-    <div className="navigation">
-      <div className="navbar">
-        <div className="logo">
-          <img src="src/assets/logo.png" />
+  const [sidebarActive, setSidebarActive] = useState(false);
 
-          <nav>
-            <ul>
-              <li>
-                <Link to="/">Home</Link>
-              </li>
-              <li>
-                <Link to="books">Books</Link>
-              </li>
-              <li>
-                <Link to="about">About us</Link>
-              </li>
-              <li>
-                <Link to="contact">Contact</Link>
-              </li>
-              <li>
-                <Link to="login">Log In</Link>
-              </li>
-            </ul>
-          </nav>
-        </div>
+  const toggleSidebar = () => {
+    setSidebarActive(!sidebarActive);
+  };
+
+  return (
+    <nav>
+      <img src="logo.png" alt="logo" />
+
+      <input
+        type="checkbox"
+        id="sidebar-active"
+        checked={sidebarActive}
+        onChange={toggleSidebar}
+      />
+      <label htmlFor="sidebar-active" className="open-sidebar-button">
+        <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32">
+          <path fill="#FFFFFF" d="M120-240v-80h720v80H120Zm0-200v-80h720v80H120Zm0-200v-80h720v80H120Z" />
+        </svg>
+      </label>
+
+      <div className={sidebarActive ? 'links-container active' : 'links-container'}>
+        <label htmlFor="sidebar-active" className="close-sidebar-button">
+          <svg xmlns="http://www.w3.org/2000/svg" height="32" viewBox="0 -960 960 960" width="32">
+            <path fill="#FFFFFF" d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+          </svg>
+        </label>
+
+        <Link className="home-link" to="/">Home</Link>
+        <Link className="book-link" to="/books">Books</Link>
+        <Link className="about-link" to="/about">About us</Link>
+        <Link className="contact-link" to="/contact">Contact</Link>
+        <Link className="login-link" to="/login">Log In</Link>
       </div>
-    </div>
+    </nav>
   );
 };
+
+export default Navbar;
